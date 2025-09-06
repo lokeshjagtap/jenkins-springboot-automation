9b2f55ea0e7d44bc32e60a149dde810359e8ce53
Your Name <you@example.com>
2025-09-06 18:26:41 +0530

    Configured SpringBootServletInitializer for external Tomcat deployment

--- src/main/java/com/example/demo/SpringBootWebAppApplication.java
+++ src/main/java/com/example/demo/SpringBootWebAppApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebAppApplication {
+public class SpringBootWebAppApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootWebAppApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebAppApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebAppApplication.class, args);
+    }
 }
